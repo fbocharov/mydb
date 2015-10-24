16b80af0dc096391d5f4dd7db6015f5e631202cd
Fyodor <bocharovfedor@gmail.com>
2015-10-25 02:42:10 +0300

    Implemented class Page.
    This represents one page (4K bytes) from disk.

--- src/backend/Page.h
+++ src/backend/Page.h
@@ -2,21 +2,37 @@
 #define Page_h
 
 #include <cstddef>
+#include <cstdint>
+
+typedef std::uint32_t PageID;
+static PageID constexpr INVALID_PAGE_ID = -1;
 
 class Page {
 public:
+	static size_t constexpr PAGE_SIZE = 4096;
+
+public:
+	Page(PageID id, char const * data = nullptr);
+
+	char const * GetData() const;
+	char * GetData();
+
 	void SetDirty();
 	void Pin();
 	void Unpin();
 
 	bool IsPinned() const;
 	bool IsDirty() const;
-	size_t GetID() const;
+	PageID GetID() const;
+	PageID GetPrevPageID() const;
+	PageID GetNextPageID() const;
+	size_t GetSpaceOffset() const;
 
 private:
+	char m_data[PAGE_SIZE];
+	PageID m_id;
 	bool m_isDirty;
 	bool m_isPinned;
-	size_t m_id;
 };
 
 #endif // Page_h
