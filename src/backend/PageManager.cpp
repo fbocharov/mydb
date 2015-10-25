2134fed6172ba55848c4ddfd76253ccc3500e504
Fyodor <bocharovfedor@gmail.com>
2015-10-25 03:05:58 +0300

    Implemented PageManager.
    This class provides pages memory management and pages caching.

---
+++ src/backend/PageManager.cpp
@@ -0,0 +1,84 @@
+#include <cassert>
+
+#include <utils/Log.h>
+
+#include "Page.h"
+#include "PageManager.h"
+
+PageManager::PageManager(std::string const & dbFileName, bool dbFileExist)
+	: m_heapFile(dbFileName, dbFileExist)
+{}
+
+PageManager::~PageManager() {
+	for (auto const & it: m_pageCache)
+		FlushPage(it.second);
+}
+
+std::weak_ptr<Page> PageManager::AllocatePage() {
+	if (MAX_PAGE_CACHE_SIZE == m_pageCache.size())
+		PopLRUPage();
+	auto pageID = m_heapFile.GetFreePageID();
+	auto page = std::make_shared<Page>(pageID);
+	m_pageCache[pageID] = page;
+	UpdateAccessTime(page);
+
+	return page;
+}
+
+void PageManager::DeallocatePage(PageID id) {
+	m_heapFile.DeallocatePage(id);
+}
+
+std::weak_ptr<Page> PageManager::GetPage(PageID id) {
+	Log(LogType::Debug) << "Request for page with id = " << id << std::endl;
+
+	auto it = m_pageCache.find(id);
+	if (m_pageCache.end() == it) {
+		if (MAX_PAGE_CACHE_SIZE == m_pageCache.size())
+			PopLRUPage();
+		auto page = std::make_shared<Page>(id);
+		m_heapFile.ReadPage(page);
+		it = m_pageCache.emplace(id, page).first;
+	}
+
+	auto page = it->second;
+	UpdateAccessTime(page);
+
+	return page;
+}
+
+void PageManager::FlushPage(std::shared_ptr<Page> page) {
+	if (page->IsDirty())
+		m_heapFile.WritePage(page);
+}
+
+void PageManager::PopLRUPage() {
+	for (auto const & it: m_pagesOrderByAccessTime)
+		if (!it.second->IsPinned()) {
+			RemovePage(it.first, it.second);
+			return;
+		}
+	// If all pages are pinned -> just remove lru.
+	auto it = m_pagesOrderByAccessTime.begin();
+	RemovePage(it->first, it->second);
+}
+
+void PageManager::RemovePage(TimePoint const & tp, std::shared_ptr<Page> page) {
+	m_pageCache.erase(page->GetID());
+	m_pagesOrderByAccessTime.erase(tp);
+	m_pagesLastAccessTime.erase(page);
+	FlushPage(page);
+}
+
+void PageManager::UpdateAccessTime(std::shared_ptr<Page> page) {
+	auto const now = std::chrono::system_clock::now();
+
+	auto it = m_pagesLastAccessTime.find(page);
+	if (m_pagesLastAccessTime.end() != it) {
+		auto lastAccess = it->second;
+		m_pagesOrderByAccessTime.erase(lastAccess);
+		m_pagesOrderByAccessTime[now] = page;
+	}
+	// Need to optimize second search?
+	m_pagesLastAccessTime[page] = now;
+}
---
+++ src/backend/PageManager.h
@@ -0,0 +1,40 @@
+#ifndef PageManager_h
+#define PageManager_h
+
+#include <map>
+#include <memory>
+#include <chrono>
+#include <cstdint>
+
+#include "HeapFile.h"
+
+class Page;
+
+class PageManager {
+public:
+	PageManager(std::string const & dbFileName, bool dbFileExist);
+	~PageManager();
+
+	std::weak_ptr<Page> AllocatePage();
+	void DeallocatePage(PageID id);
+	std::weak_ptr<Page> GetPage(PageID id);
+
+private:
+	typedef std::chrono::system_clock::time_point TimePoint;
+
+private:
+	void FlushPage(std::shared_ptr<Page> page);
+	void PopLRUPage();
+	void RemovePage(TimePoint const & tp, std::shared_ptr<Page> page);
+	void UpdateAccessTime(std::shared_ptr<Page> page);
+
+private:
+	std::map<PageID, std::shared_ptr<Page>> m_pageCache;
+	HeapFile m_heapFile;
+	std::map<TimePoint, std::shared_ptr<Page>> m_pagesOrderByAccessTime;
+	std::map<std::shared_ptr<Page>, TimePoint> m_pagesLastAccessTime;
+
+	static size_t constexpr MAX_PAGE_CACHE_SIZE = 128;
+};
+
+#endif // PageManager_h
