49e705579e12cc15bc8293d305d56ff26cc2a330
Fyodor <bocharovfedor@gmail.com>
2015-10-26 21:34:55 +0300

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
-int main() {
-	std::cout << "Hello world" << std::endl;
+#include <parser/SQLParser.h>
+#include <parser/SQLParserException.h>
+#include <utils/Log.h>
+
+int main(int argc, char * argv[]) {
+	Log::SetStream(LogType::Debug, std::cout);
+	Log::SetStream(LogType::Error, std::cerr);
+
+	Log(LogType::Info) << "Start listening to unput." << std::endl;
+
+	std::string userInput;
+	std::cout << "Hello from MyDB!" << std::endl;
+	while (true) {
+		std::cout << "> ";
+		std::getline(std::cin, userInput);
+		try {
+			auto statement = SQLParser::Instance().ParseStatement(userInput);
+		} catch (SQLParserException const & e) {
+			std::cerr << "ERROR: " << e.what() << std::endl;
+			Log(LogType::Error) << "Exception while parsing " << userInput << ": " << e.what() << std::endl;
+		}
+	}
+
 	return 0;
 }
