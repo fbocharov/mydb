3847fdb5268871ab802a6c09166a98829cd9fb3e
Fyodor <bocharovfedor@gmail.com>
2015-11-28 19:14:48 +0300

    Implemented class Table.

--- src/core/Table.h
+++ src/core/Table.h
@@ -1,22 +1,35 @@
 #ifndef Table_h
 #define Table_h
 
-#include <vector>
+#include <map>
+#include <string>
+#include <memory>
 
+#include "DataPage.h"
 #include "Common.h"
+
+class ICursor;
 
 class Table {
 public:
-	Table();
-	~Table();
+	Table(PageManager & manager, ColumnDescriptors const & descriptors);
 
-	void Insert();
-	void Delete(); // accepts iterator on deleting object
+	static Table Deserialize(Page const & page, PageManager & manager);
+	void Serialize(Page & page);
 
-	void GetIterator();
+	void Insert(std::map<std::string, std::string> const & colVals);
+	std::unique_ptr<ICursor> GetCursor();
 
 private:
-	std::vector<ColumnDescriptor> m_columnDescriptors;
+	Table(ColumnDescriptors const & columnDescriptors, PageManager & pageManager, PageID firstPage);
+
+	void AddPage();
+
+private:
+	ColumnDescriptors m_columnDescriptors;
+	PageManager & m_pageManager;
+	PageID m_firstPageID;
+	std::unique_ptr<DataPage> m_pageWithSpace;
 };
 
 #endif // Table_h
