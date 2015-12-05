9ff12bc4f7ffe5e074d22e646100464668c82300
Fyodor <bocharovfedor@gmail.com>
2015-12-05 07:08:04 +0300

    Ignoring build dir when doing static analysis.

--- analyze_static.sh
+++ analyze_static.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASE_FLAGS="--force --language=c++ --std=c++11 -i ./src/sql"
+BASE_FLAGS="--force --language=c++ --std=c++11 -i ./src/sql -i ./build"
 
 cppcheck -j 4 --enable=all $BASE_FLAGS .
 cppcheck --enable=unusedFunction $BASE_FLAGS .
