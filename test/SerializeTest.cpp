baf7b19743b9e18e3d52da7a5a02bb1b450c27ce
Fyodor <bocharovfedor@gmail.com>
2015-12-05 04:09:55 +0300

    Added little tests for serialize/deserialize code.

---
+++ test/SerializeTest.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+
+#include <cstring>
+
+#include <core/Common.h>
+
+TEST(SerializeTest, ColumnDescriptorSerializeTest) {
+	char const * name = "users";
+	char buf[ColumnDescriptor::DESCRIPTOR_SIZE];
+
+	ColumnDescriptor before;
+	before.size = strlen(name);
+	memcpy(before.name, name, before.size);
+	before.type = FieldType::VARCHAR;
+	before.Serialize(buf);
+
+	ColumnDescriptor after = ColumnDescriptor::Deserialize(buf);
+	ASSERT_EQ(0, strcmp(before.name, after.name));
+	ASSERT_EQ(before.type, after.type);
+	ASSERT_EQ(before.size, after.size);
+
+	memset(buf, 0, ColumnDescriptor::DESCRIPTOR_SIZE);
+	before.size = strlen(name);
+	memcpy(before.name, name, before.size);
+	before.type = FieldType::VARCHAR;
+	before.Serialize(buf);
+
+	after = ColumnDescriptor::Deserialize(buf);
+	ASSERT_EQ(0, strcmp(before.name, after.name));
+	ASSERT_EQ(before.type, after.type);
+	ASSERT_EQ(before.size, after.size);
+}
