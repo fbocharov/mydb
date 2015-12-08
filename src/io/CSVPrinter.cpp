394d83cd6e0fd5164b9ea56ff566f0a1ee5448f5
Fyodor <bocharovfedor@gmail.com>
2015-12-08 13:45:17 +0300

    Removed last comma when printing select results.

--- src/io/CSVPrinter.cpp
+++ src/io/CSVPrinter.cpp
@@ -1,3 +1,5 @@
+#include <sstream>
+
 #include "CSVPrinter.h"
 
 CSVPrinter::CSVPrinter(std::ostream & stream)
@@ -5,14 +7,21 @@ CSVPrinter::CSVPrinter(std::ostream & stream)
 {}
 
 void CSVPrinter::PrintHeading(ColumnDescriptors const & descriptors) {
-	for (auto const & desc: descriptors)
-		m_stream << desc.name << "(" << GetTypeStr(desc.type) << "),";
+	for (size_t i = 0; i < descriptors.size(); ++i) {
+		auto const & desc = descriptors[i];
+		m_stream << desc.name << "(" << GetTypeStr(desc.type) << ")";
+		if (i + 1 != descriptors.size())
+			m_stream << ",";
+	}
 	m_stream << std::endl;
 }
 
 void CSVPrinter::PrintLine(std::vector<std::string> const & values) {
-	for (auto const & v: values)
-		m_stream << v << ",";
+	for (size_t i = 0; i < values.size(); ++i) {
+		m_stream << values[i];
+		if (i + 1 != values.size())
+			m_stream << ",";
+	}
 	m_stream << std::endl;
 }
 
