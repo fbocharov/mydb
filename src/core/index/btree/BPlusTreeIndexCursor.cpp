1aad808ac82e756901ce0f4a081048e6ab981ef5
Fyodor <bocharovfedor@gmail.com>
2016-01-27 16:23:40 +0300

    Stubs for index cursor which fix build.

--- src/core/index/btree/BPlusTreeIndexCursor.cpp
+++ src/core/index/btree/BPlusTreeIndexCursor.cpp
@@ -2,39 +2,23 @@
 #include <core/DataPage.h>
 
 #include "BPlusTreeIndexCursor.h"
-#include "LeafNode.h"
 
-BPlusTreeIndexCursor::BPlusTreeIndexCursor(PageManager & manager, PageID startPageID, size_t keySize,
-		ColumnDescriptors const & descriptors, Condition const & indexCondition)
-	: DeleteCursor(descriptors)
-	, m_pageManager(manager)
-	, m_indexKeySize(keySize)
+BPlusTreeIndexCursor::BPlusTreeIndexCursor(ColumnDescriptors const & descriptors, PageManager & manager)
+	: CursorImpl(descriptors)
 {
-	m_iterator = std::make_unique<LeafNode::EntryIterator>(
-		LeafNode(m_pageManager, startPageID, m_indexKeySize).GetEntryIterator());
-	// moving iterator to first entry
-	while (!indexCondition.Satisfies(m_iterator->GetKey()))
-		++(*m_iterator);
+}
+
+bool BPlusTreeIndexCursor::Delete() {
+
 }
 
 bool BPlusTreeIndexCursor::HasNext() const {
-	return m_iterator->IsValid() ||
-		m_iterator->GetNextLeafPageID() != INVALID_PAGE_ID;
+
 }
 
 char const * BPlusTreeIndexCursor::GetCurrentRecord() const {
-	PageID pageID = m_iterator->GetRecordPageID();
-	std::uint32_t recordNumber = m_iterator->GetRecordNumber();
-	// TODO: this ctor call must be optimized later.
-	return DataPage(m_pageManager, pageID, m_descriptors).GetRawRecord(recordNumber);
 }
 
 void BPlusTreeIndexCursor::GoToNextRecord() {
-	if (m_iterator->IsValid())
-		++(*m_iterator);
-	else {
-		PageID nextLeafPageID = m_iterator->GetNextLeafPageID();
-		m_iterator = std::make_unique<LeafNode::EntryIterator>(
-			LeafNode(m_pageManager, nextLeafPageID, m_indexKeySize).GetEntryIterator());
-	}
+
 }
--- src/core/index/btree/BPlusTreeIndexCursor.h
+++ src/core/index/btree/BPlusTreeIndexCursor.h
@@ -3,25 +3,18 @@
 
 #include <backend/PageManager.h>
 #include <common/Condition.h>
+#include <core/CursorImpl.h>
 
-#include "LeafNode.h"
-#include <core/DeleteCursor.h>
-
-class BPlusTreeIndexCursor : public DeleteCursor {
+class BPlusTreeIndexCursor : public CursorImpl {
 public:
-	BPlusTreeIndexCursor(PageManager & manager, PageID startPageID, size_t keySize,
-		ColumnDescriptors const & descriptors, Condition const & indexCondition);
+	BPlusTreeIndexCursor(const ColumnDescriptors &descriptors, PageManager & manager);
 
-	virtual bool HasNext() const;
+	virtual bool Delete() override;
 
 private:
+	virtual bool HasNext() const override;
 	virtual char const * GetCurrentRecord() const override;
 	virtual void GoToNextRecord() override;
-
-private:
-	PageManager & m_pageManager;
-	std::unique_ptr<LeafNode::EntryIterator> m_iterator;
-	size_t m_indexKeySize;
 };
 
 #endif // BPlusTreeIndexCursor_h
